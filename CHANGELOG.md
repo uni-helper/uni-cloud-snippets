b56d6a172d3c5c2a3a020427374cd58ecba6a524
ModyQyW <wurui-dev@foxmail.com>
2022-11-15 11:24:41 +0800

    chore: rebrand

---
+++ CHANGELOG.md
@@ -0,0 +1,47 @@
+# 改动日志
+
+## 0.2.8
+
+- 切换到 [uni-helper](https://github.com/uni-helper) 组织下
+
+## 0.2.7
+
+- 发布流程改进
+- 更新链接
+
+## 0.2.6
+
+- 发布流程改进
+
+## 0.2.5
+
+- 增加适用的 language。
+
+## 0.2.4
+
+- 修复 README 链接。
+
+## 0.2.3
+
+- 修复 README 链接。
+
+## 0.2.2
+
+- 修复 README 链接。
+
+## 0.2.1
+
+- 更优雅地使用脚本生成 README
+
+## 0.2.0
+
+- 增加 api
+- 修复部分 api 代码块问题
+- 使用 node 生成 README.md
+
+## 0.1.0
+
+- uni-cloud 基本能力的代码片段，包括组件和 API
+- 支持 vue2
+- 参考 [uni-cloud 官方文档](https://uniapp.dcloud.net.cn/uniCloud/README)
+- 参考 [Vue.js 风格指南](https://cn.vuejs.org/v2/style-guide/index.html)
---
+++ scripts/generate.ts
@@ -0,0 +1,102 @@
+import { readFileSync, writeFileSync } from 'node:fs';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const root = resolve(__dirname, '..');
+
+const htmlObject = JSON.parse(
+  readFileSync(resolve(root, 'snippets', 'vue-html.json'), { encoding: 'utf8' }),
+);
+const javascriptObject = JSON.parse(
+  readFileSync(resolve(root, 'snippets', 'javascript.json'), { encoding: 'utf8' }),
+);
+
+let readme = `# @uni-helper/uni-cloud-snippets-vscode
+
+[![License](https://img.shields.io/github/license/uni-helper/uni-cloud-snippets-vscode)](https://github.com/uni-helper/uni-cloud-snippets-vscode/blob/main/LICENSE)
+
+[![VSCode](https://vsmarketplacebadge.apphb.com/version-short/uni-helper.uni-cloud-snippets-vscode.svg)](https://marketplace.visualstudio.com/items?itemName=uni-helper.uni-cloud-snippets-vscode)
+
+[![OpenVSX](https://img.shields.io/badge/dynamic/json?color=brightgreen&label=OpenVSX&query=%24.version&url=https%3A%2F%2Fopen-vsx.org%2Fapi%2Funi-helper%2Funi-cloud-snippets-vscode)](https://open-vsx.org/extension/uni-helper/uni-cloud-snippets-vscode)
+
+[改动日志](https://github.com/uni-helper/uni-cloud-snippets-vscode/blob/main/CHANGELOG.md)
+
+## 插件特性
+
+- uni-cloud 基本能力代码片段
+- 参考 [uni-cloud 官方文档](https://uniapp.dcloud.net.cn/uniCloud/)
+- 参考 [Vue.js 2 风格指南](https://v2.cn.vuejs.org/v2/style-guide/) 和 [Vue.js 3 风格指南](https://cn.vuejs.org/style-guide/)
+
+**插件和文档的冲突之处，请以文档为准。**
+
+插件源代码在 [uni-helper/uni-cloud-snippets-vscode](https://github.com/uni-helper/uni-cloud-snippets-vscode)。欢迎提交 ISSUE 和 PR 改进本插件。
+
+## 使用
+
+安装插件后重启 VSCode 即可。
+
+`;
+
+// 添加 HTML
+readme += '## HTML\n\n';
+readme += '|API|Prefix|Description|\n|-|-|-|\n';
+for (const key of Object.keys(htmlObject)) {
+  const { prefix, body, description } = htmlObject[key];
+  let newPrefix = '';
+  for (const text of prefix) {
+    newPrefix += `\`${text}\`, `;
+  }
+  newPrefix = newPrefix.slice(0, -2);
+  let newBody = '';
+  newBody = `\`${body[0]
+    .replace(/\b .*[/>]/g, '')
+    .replace(/\(?\([\w "$'(),/:=>{|}]+/g, '()')
+    .replace(/\$\d[\w/<>-]*/g, '')}`;
+  if (newBody.includes('/* ') && !newBody.includes(' */')) {
+    newBody += ' */`';
+  } else if (newBody.includes('<!-- ') && !newBody.includes(' -->')) {
+    newBody += ' -->`';
+  } else if (newBody.includes('<') && !newBody.includes('>')) {
+    newBody += '>`';
+  } else {
+    newBody += '`';
+  }
+  readme += `|${newBody}|${newPrefix}|${description}|\n`;
+}
+readme += '\n';
+
+// 添加 JavaScript/TypeScript
+readme += '## JavaScript/TypeScript\n\n';
+readme += '|API|Prefix|Description|\n|-|-|-|\n';
+for (const key of Object.keys(javascriptObject)) {
+  const { prefix, body, description } = javascriptObject[key];
+  let newPrefix = '';
+  for (const text of prefix) {
+    newPrefix += `\`${text}\`, `;
+  }
+  newPrefix = newPrefix.slice(0, -2);
+  let newBody = '';
+  newBody = `\`${body[0]
+    .replace(/\b .*[/>]/g, '')
+    .replace(/\(?\([\w "$'(),/:=>{|}]+/g, '()')
+    .replace(/\$\d[\w/<>-]*/g, '')}`;
+  if (newBody.includes('/* ') && !newBody.includes(' */')) {
+    newBody += ' */`';
+  } else if (newBody.includes('<!-- ') && !newBody.includes(' -->')) {
+    newBody += ' -->`';
+  } else if (newBody.includes('<') && !newBody.includes('>')) {
+    newBody += '>`';
+  } else {
+    newBody += '`';
+  }
+  readme += `|${newBody}|${newPrefix}|${description}|\n`;
+}
+readme += '\n';
+
+readme += `## 额外推荐
+
+请查看 [uni-helper 插件说明](https://marketplace.visualstudio.com/items?itemName=uni-helper.uni-helper-vscode)。
+`;
+
+writeFileSync(resolve(root, 'README.md'), readme);
